72c162f83acde1a34bda75db814c49deaa18f237
thepape <romain.papelier@gmail.com>
2015-04-21 19:29:41 +0200

    disposition des element css amelioree

--- Ammu-nation/Projet_CSI/View/html/notreDrive.php
+++ Ammu-nation/Projet_CSI/View/html/notreDrive.php
@@ -28,15 +28,19 @@ if(isset($_GET['categ'])) {
 </head>
 
 <body>
-<div class="Entete">
-	<h1> Ammu-nation </h1>
-</div>
-
+<header>
 <div class ="Infos_user">
 	<p> Connexion -> AJAX et inscription </p>
 	<p> A la connexion du client, créer automatiquement une commande, ou récupérer l'ancienne </p>
 </div>
 
+<div class="Entete">
+	<h1> Ammu-nation </h1>
+</div>
+
+
+</header>
+<div id="contenu">
 <div class="Colonne_categ">
 <?php
 	ControllerCategorie::ChoixCategorie();
@@ -55,6 +59,13 @@ if(isset($_GET['categ'])) {
 		}
 	}
 ?>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a dignissim erat. Phasellus ac lacus convallis, aliquam sapien id, euismod lacus. Maecenas urna nulla, fringilla vitae massa vel, rhoncus dapibus lacus. Mauris consequat luctus ante. Aenean ac augue magna. Nam libero diam, laoreet sit amet lacus in, sodales viverra massa. Praesent sit amet justo magna. Mauris finibus ex a purus laoreet viverra. Sed eget eros nec est vehicula ornare auctor id lacus. Ut ut nibh et erat condimentum dictum ut et tortor. Integer fermentum arcu in facilisis faucibus. Quisque lorem metus, volutpat hendrerit ligula id, commodo feugiat lectus. Vivamus scelerisque cursus nisl, et varius odio placerat a. Quisque et lobortis justo. Nam laoreet, enim in vestibulum bibendum, arcu nulla finibus orci, in vestibulum lectus nunc blandit augue. Vestibulum ut neque vestibulum neque lacinia mollis.
+
+
+Vestibulum sit amet libero sagittis metus sollicitudin finibus at nec magna. Donec auctor imperdiet mauris, aliquet imperdiet elit rhoncus eget. Cras non lorem id metus ultrices aliquam. Aenean elementum ipsum a neque convallis suscipit. Nullam aliquam mauris et lorem finibus suscipit. Integer et mattis dolor. Vivamus ligula ex, dignissim non scelerisque nec, rutrum eu nisi. Mauris dictum leo nunc. Maecenas eu tempus ligula, vestibulum elementum erat. Nullam maximus, augue ac condimentum maximus, lectus purus ultricies odio, malesuada dictum justo nisl at nisi. Pellentesque id dolor maximus, luctus sapien ut, vulputate ex. Duis ac augue condimentum, rutrum ante eget, accumsan eros. Aenean venenatis ultrices tincidunt. Etiam vitae diam non turpis condimentum suscipit. Donec volutpat nunc in lacinia semper.</p>
+</div>
+
+
 </div>
 
 
